490d1d68b820cdaa089d7fb8179ec94d07a638fb
NaSuRall <romain.poulain@edu.esiee-it.fr>
2026-03-06 08:45:36 +0000

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API running" });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
