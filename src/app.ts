3ff24c43f1d5e10e4e3a58c1d37aaa88daf32c1c
samuel <samuel@Mac-CF-23-002.local>
2026-03-09 09:43:32 +0100

    config : init structure

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express"
+import userRoutes from "./modules/user/user.routes"
+
+const app = express()
+
+app.use(express.json())
+
+app.use("/users", userRoutes)
+
+export default app
